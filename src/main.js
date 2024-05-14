19eaf6f49b12a4b214024513d4408dcb29a5cf23
jp-quintana <juanquintana1996@gmail.com>
2024-05-14 19:06:49 -0300

    wip

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Phaser from './lib/phaser.js';
+import { SCENE_KEYS } from './scenes/scene-keys.js';
+import { PreloadScene } from './scenes/preload-scene.js';
+
+const game = new Phaser.Game({
+  parent: 'game-container',
+  // scene: [PreloadScene],
+});
+
+game.scene.add(SCENE_KEYS.PRELOAD_SCENE, PreloadScene);
+// game.scene.start(SCENE_KEYS.PRELOAD_SCENE);
