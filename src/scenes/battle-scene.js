b5212881672c445ccaec06b9f8926e74e3364bff
jp-quintana <juanquintana1996@gmail.com>
2024-07-18 15:50:35 -0300

    wip

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -37,14 +37,68 @@ export class BattleScene extends Phaser.Scene {
         .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
         .setOrigin(0),
       playerMonsterName,
-      // this.#createHealth(34, 34)
+      this.#createHealth(34, 34),
+      this.add.text(playerMonsterName.width + 35, 23, 'L5', {
+        color: '#ED474B',
+        fontSize: '28px ',
+      }),
+      this.add.text(30, 55, 'HP', {
+        color: '#FF6505',
+        fontSize: '24px',
+        fontStyle: 'italic',
+      }),
+      this.add
+        .text(443, 80, '25/25', {
+          color: '#7E3D4F',
+          fontSize: '16',
+        })
+        .setOrigin(1, 0), // align to the right always, numbers might go up as monster levels up, so you want text to grow towards left not right
+    ]);
+
+    // render out the enemy and health bar
+    const enemyMonsterName = this.add.text(
+      30,
+      20,
+      MONSTER_ASSET_KEYS.CARNODUSK,
+      {
+        color: '#7E3D3F',
+        fontSize: '32px ',
+      }
+    );
+    this.add.container(0, 0, [
+      this.add
+        .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+        .setOrigin(0)
+        .setScale(1, 0.8),
+      enemyMonsterName,
+      this.#createHealth(34, 34),
+      this.add.text(enemyMonsterName.width + 35, 23, 'L5', {
+        color: '#ED474B',
+        fontSize: '28px ',
+      }),
+      this.add.text(30, 55, 'HP', {
+        color: '#FF6505',
+        fontSize: '24px',
+        fontStyle: 'italic',
+      }),
     ]);
   }
 
-  // #createHealth(x, y) {
-  //   const leftCap = this.add.image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP);
-  //   const rightCap = this.add.image(x, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP);
-  //   const middle = this.add.image(x, y, HEALTH_BAR_ASSET_KEYS.MIDDLE);
-  //   return this.add.container(x, y, [leftCap, middle, rightCap]);
-  // }
+  #createHealth(x, y) {
+    const scale = [1, 0.7];
+    const leftCap = this.add
+      .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
+      .setOrigin(0, 0.5)
+      .setScale(...scale);
+    const middle = this.add
+      .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
+      .setOrigin(0, 0.5)
+      .setScale(...scale);
+    middle.displayWidth = 360;
+    const rightCap = this.add
+      .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
+      .setOrigin(0, 0.5)
+      .setScale(...scale);
+    return this.add.container(x, y, [leftCap, middle, rightCap]);
+  }
 }
