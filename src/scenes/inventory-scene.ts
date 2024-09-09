ce707c4ec5ed464ea33fb21fe1aedcbc8ef933f6
jp-quintana <juanquintana1996@gmail.com>
2024-09-09 15:05:12 -0300

    ep 82

--- src/scenes/inventory-scene.ts
+++ src/scenes/inventory-scene.ts
@@ -4,6 +4,25 @@ import { NineSlice } from '../utils/nine-slice';
 import { BaseScene } from './base-scene';
 import { SCENE_KEYS } from './scene-keys';
 
+export interface InventoryItem {
+  name: string;
+  description: string;
+  quantity: number;
+  gameObjects: {
+    itemName?: Phaser.GameObjects.Text;
+    quantity?: Phaser.GameObjects.Text;
+    quantitySign?: Phaser.GameObjects.Text;
+  };
+}
+
+const CANCEL_TEXT_DESCRIPTION = 'Close your bag, and go back to adventuring!';
+
+const INVENTORY_ITEM_POSITION = Object.freeze({
+  x: 50,
+  y: 14,
+  space: 50,
+});
+
 const INVENTORY_TEXT_STYLE = Object.freeze({
   fontFamily: KENNEY_FUTURE_NARROW_FONT_NAME,
   color: '#000000',
@@ -13,6 +32,10 @@ const INVENTORY_TEXT_STYLE = Object.freeze({
 export class InventoryScene extends BaseScene {
   private sceneData: { previousSceneName: SCENE_KEYS };
   private nineSliceMainContainer: NineSlice;
+  private selectedInventoryDescriptionText: Phaser.GameObjects.Text;
+  private userInputCursor: Phaser.GameObjects.Image;
+  private inventory: InventoryItem[];
+  private selectedInventoryOptionIndex: number;
 
   constructor() {
     super({ key: SCENE_KEYS.INVENTORY_SCENE });
@@ -27,6 +50,18 @@ export class InventoryScene extends BaseScene {
       textureManager: this.sys.textures,
       assetKeys: [UI_ASSET_KEYS.MENU_BACKGROUND],
     });
+
+    this.inventory = [
+      {
+        name: 'potion',
+        description:
+          'A basic healing item that will heal 30 HP from a single monster',
+        quantity: 10,
+        gameObjects: {},
+      },
+    ];
+
+    this.selectedInventoryOptionIndex = 0;
   }
 
   create() {
@@ -51,18 +86,96 @@ export class InventoryScene extends BaseScene {
       .rectangle(4, 4, 692, 352, 0xffff88)
       .setOrigin(0)
       .setAlpha(0.6);
-
     container.add(containerBackground);
+
     const titleContainerBackground = this.add
       .rectangle(4, 4, 232, 56, 0xffff88)
       .setOrigin(0)
       .setAlpha(0.6);
-
     titleContainer.add(titleContainerBackground);
 
     const textTitle = this.add
       .text(116, 28, 'Items', INVENTORY_TEXT_STYLE)
       .setOrigin(0.5);
     titleContainer.add(textTitle);
+
+    // create inventory text from available items
+    this.inventory.forEach((item, i) => {
+      const itemText = this.add.text(
+        INVENTORY_ITEM_POSITION.x,
+        INVENTORY_ITEM_POSITION.y + i * INVENTORY_ITEM_POSITION.space,
+        item.name,
+        INVENTORY_TEXT_STYLE
+      );
+      titleContainer.add(textTitle);
+      const qty1Text = this.add.text(
+        620,
+        INVENTORY_ITEM_POSITION.y + i * INVENTORY_ITEM_POSITION.space + 2,
+        'x',
+        {
+          color: '#000000',
+          fontSize: '30px',
+          fontStyle: 'bold',
+        }
+      );
+      titleContainer.add(textTitle);
+      const qty2Text = this.add.text(
+        650,
+        INVENTORY_ITEM_POSITION.y + i * INVENTORY_ITEM_POSITION.space,
+        `${item.quantity}`,
+        INVENTORY_TEXT_STYLE
+      );
+      container.add([itemText, qty1Text, qty2Text]);
+      item.gameObjects = {
+        itemName: itemText,
+        quantity: qty2Text,
+        quantitySign: qty1Text,
+      };
+    });
+
+    // create cancel text
+    const cancelText = this.add.text(
+      INVENTORY_ITEM_POSITION.x,
+      INVENTORY_ITEM_POSITION.y +
+        this.inventory.length * INVENTORY_ITEM_POSITION.space,
+      'Cancel',
+      INVENTORY_TEXT_STYLE
+    );
+    container.add(cancelText);
+
+    // create player input cursor
+    this.userInputCursor = this.add
+      .image(30, 30, UI_ASSET_KEYS.CURSOR)
+      .setScale(3);
+
+    container.add(this.userInputCursor);
+
+    // create inventory description text
+    this.selectedInventoryDescriptionText = this.add.text(25, 420, '', {
+      ...INVENTORY_TEXT_STYLE,
+      ...{
+        wordWrap: {
+          width: this.scale.width - 18,
+        },
+      },
+      color: '#ffffff',
+    });
+
+    this.updateItemDescriptionText();
+  }
+
+  private updateItemDescriptionText() {
+    if (this.isCancelButtonSelected()) {
+      this.selectedInventoryDescriptionText.setText(CANCEL_TEXT_DESCRIPTION);
+      return;
+    }
+
+    this.selectedInventoryDescriptionText.setText(
+      this.inventory[this.selectedInventoryOptionIndex].description
+    );
+  }
+
+  private isCancelButtonSelected() {
+    return this.selectedInventoryOptionIndex === this.inventory.length;
   }
 }
